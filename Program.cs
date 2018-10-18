0fa624df715a4cc8386cc8b7b03a7f640106f804
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-18 08:10:17 +0200

    Use Magic number

--- Program.cs
+++ Program.cs
@@ -10,12 +10,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            var player = new PlayerCharacter();
+            player.Name = "Sharaf";
+            player.DaysSinceLastLogin = 42;
+            player.DateOfBirth = new DateTime(2018, 10, 15);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            PlayerDisplayer.Write(player);
+
+            Console.ReadLine();
         }
     }
 }
