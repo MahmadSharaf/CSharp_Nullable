b9bd42de782f84b573330580075f31dd88461b5d
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-18 11:12:43 +0200

    More on Nullable Use HasValue instead of ==null Null operators: Conditional Null-Coalescing Null-conditional

--- PlayerDisplayer.cs
+++ PlayerDisplayer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace CSharp_Nullable
 {
     class PlayerDisplayer
     {
@@ -15,11 +15,25 @@ namespace ConsoleApp1
             else
                 Console.WriteLine(player.Name);
 
-            if(player.DaysSinceLastLogin == null)
-                Console.WriteLine("No value for DaysSinceLastLogin");            
-            else
-                Console.WriteLine(player.DaysSinceLastLogin);
-            
+            //?Null-checking Operators:
+            //!Conditional operator
+            //        Checks if it is null                  if false                        if true
+            //x int days = player.DaysSinceLastLogin.HasValue ? player.DaysSinceLastLogin.Value : -1;
+            //!Null-Coalescing operator
+            //        Checks if it is null          if true
+            //x int days = player.DaysSinceLastLogin ?? -1;
+            //!Null-conditional operator
+            // Checks if player is null, if true return null, if not checks if DaysSinceLastLogin is null, if true returns null 
+            int? days = player?.DaysSinceLastLogin;
+
+            //x int days = player.DaysSinceLastLogin.GetValueOrDefault(); //GetValueOrDefault return the default value or it can be overridden by an input value to the method
+            Console.WriteLine($"{days} days since last login");
+
+            //x if(player.DaysSinceLastLogin.HasValue)      //HasValue is a boolean check which returns whether it has a value or not
+            //x     Console.WriteLine(player.DaysSinceLastLogin);                        
+            //x else
+            //x     Console.WriteLine("No value for DaysSinceLastLogin");
+
             if (player.DateOfBirth == null)
                 Console.WriteLine("No Date of birth specified");            
             else
--- Program.cs
+++ Program.cs
@@ -4,18 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace CSharp_Nullable
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var player = new PlayerCharacter();
+            PlayerCharacter[] players = new PlayerCharacter[3]
+            {
+                new PlayerCharacter {Name = "Sarah"},
+                new PlayerCharacter(),
+                null
+            };
+
+            //Null-conditional with arrays
+            string p1 = players?[0]?.Name;
+            string p2 = players?[1]?.Name;
+            string p3 = players?[2]?.Name;
+
             //player.Name = "Sharaf";
             //player.DaysSinceLastLogin = 42;
             //player.DateOfBirth = new DateTime(2018, 10, 15);
 
-            PlayerDisplayer.Write(player);
+            //PlayerDisplayer.Write(player);
 
             //Console.ReadLine();
         }
